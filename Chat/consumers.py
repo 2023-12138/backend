c08aa01bd57d1c646e85fbb47c0079264cbb2654
Zzzzzzy123 <1274975655@qq.com>
2023-08-25 15:06:14 +0800

    update

--- Chat/consumers.py
+++ Chat/consumers.py
@@ -4,39 +4,23 @@ import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
+userSocketDict = {}
 
 class ChatConsumer(WebsocketConsumer):
+    uid = -1
     def connect(self):
-        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        self.room_group_name = f"chat_{self.room_name}"
-
-        # Join room group
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name, self.channel_name
-        )
-
+        self.uid = int(self.scope["url_route"]["kwargs"]["uid"])
+        userSocketDict[self.uid] = self
         self.accept()
 
     def disconnect(self, close_code):
-        # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name, self.channel_name
-        )
+        del userSocketDict[self.uid]
 
     # Receive message from WebSocket
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
-        print(message)
-        print("111\n")
-        # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name, {"type": "chat.message", "message": message}
-        )
-
-    # Receive message from room group
-    def chat_message(self, event):
-        message = event["message"]
-
-        # Send message to WebSocket
+        message = text_data_json["message"] #要发送的信息
+        to = int(text_data_json["to"]) #要发送给的成员id或者群id
+        toUserSocket = userSocketDict.get("to")
+        toUserSocket.send(text_data=json.dumps({"message": message}))
         self.send(text_data=json.dumps({"message": message}))
