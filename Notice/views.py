62da1dd994d30b288a74470f4e633acda588b7ce
Zzzzzzy123 <1274975655@qq.com>
2023-08-26 11:50:51 +0800

    upgrade

--- Notice/views.py
+++ Notice/views.py
@@ -64,10 +64,7 @@ def oneDelete(request):
 def getNotice(request): #返回群聊通知列表
     user = request.myUser
     uid = user.uid
-    notices = Notice.objects.filter(uid=uid)
+    notices = Notice.objects.filter(Q(uid=uid)&Q(is_active=True))
     notice_list = [notice.to_dict() for notice in notices]
-    if notice_list:
-        return JsonResponse({'code': 200, 'message': "通知获取成功", "data": {'notice_list': notice_list}})
-    else:
-        return JsonResponse({'code': 201, 'message': "无通知"})
+    return JsonResponse({'code': 200, 'message': "通知获取成功", "data": {'notice_list': notice_list}})
 
