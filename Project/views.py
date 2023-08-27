27f065965f90abfc4270b5270c94e0fcded2a1c1
unknown-zyz <805290114@qq.com>
2023-08-27 10:49:10 +0800

    add views

--- Project/views.py
+++ Project/views.py
@@ -1,6 +1,7 @@
 import json
 from django.http import JsonResponse
 from Tools.LoginCheck import loginCheck
+from django.db.models import Q
 from models import Project
 
 @loginCheck
@@ -18,3 +19,57 @@ def createProject(request):
     except:
         return JsonResponse({'code': 400, 'message': '数据库保存失败', 'data': {}})
     return JsonResponse({'code': 200, 'message': '项目创建成功', 'data': {'pid': new_project.pid}})
+
+@loginCheck
+def deleteProject(request):
+    user = request.myUser
+    json_str = request.body
+    json_obj = json.loads(json_str)
+    pid = json_obj.get('pid')  # 项目id
+    tid = json_obj.get('tid')  # 所属团队
+    data = Project.objects.get(Q(pid=pid) & Q(tid=tid))
+    try:
+        data.is_active = 0
+        data.save()
+    except:
+        return JsonResponse({'code': 400, 'message': '数据库保存失败', 'data': {}})
+    return JsonResponse({'code': 200, 'message': '项目删除成功', 'data': {}})
+
+@loginCheck
+def recoverProject(request):
+    user = request.myUser
+    json_str = request.body
+    json_obj = json.loads(json_str)
+    pid = json_obj.get('pid')  # 项目id
+    tid = json_obj.get('tid')  # 所属团队
+    data = Project.objects.get(Q(pid=pid) & Q(tid=tid))
+    try:
+        data.is_active = 1
+        data.save()
+    except:
+        return JsonResponse({'code': 400, 'message': '数据库保存失败', 'data': {}})
+    return JsonResponse({'code': 200, 'message': '项目恢复成功', 'data': {}})
+
+@loginCheck
+def renameProject(request):
+    user = request.myUser
+    json_str = request.body
+    json_obj = json.loads(json_str)
+    pid = json_obj.get('pid')
+    project_name = json_obj.get('project_name')
+    data = Project.objects.get(Q(pid=pid))
+    try:
+        data.project_name = project_name
+        data.save()
+    except:
+        return JsonResponse({'code': 400, 'message': '数据库保存失败', 'data': {}})
+    return JsonResponse({'code': 200, 'message': '项目重命名成功', 'data': {}})
+
+@loginCheck
+def getProject(request):
+    user = request.myUser
+    json_str = request.body
+    json_obj = json.loads(json_str)
+    pid = json_obj.get('pid')
+    data = Project.objects.get(Q(pid=pid))
+    JsonResponse({'code': 200, 'message': "项目获取成功", "data": {'project': data}})
