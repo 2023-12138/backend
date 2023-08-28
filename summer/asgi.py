2589b128d1cf614e7f7da26c9a842562250b6f29
Zzzzzzy123 <1274975655@qq.com>
2023-08-28 22:26:58 +0800

    upgrade

--- summer/asgi.py
+++ summer/asgi.py
@@ -14,7 +14,7 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-
+import Chat.routing
 from Chat.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "summer.settings")
@@ -22,9 +22,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "summer.settings")
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
-import Chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'summer.settings.prod')
 django.setup()
 
 application = ProtocolTypeRouter(
