5ebaa9861734a474754dda56fd79f59ea0790874
Zzzzzzy123 <1274975655@qq.com>
2023-09-01 15:50:34 +0800

    upgrade

--- User/views.py
+++ User/views.py
@@ -107,6 +107,7 @@ def userLogin(request):
         return JsonResponse({'code': 200, 'message': "登录成功", 'data': {
             'token': token,
             'uid': user.uid,
+            'logincnt': user.logincnt,
             "privateTid": privateTid
         }})
     # 重定向回先前页面
